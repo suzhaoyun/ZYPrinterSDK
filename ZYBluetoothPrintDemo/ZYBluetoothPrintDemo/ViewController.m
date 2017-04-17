8c7df0b76a5b148fe970cbb4c6576e783a5635b9
ZYSu <1014917278@qq.com>
2017-04-17 17:59:18 +0800

    完善演示代码

--- ZYBluetoothPrintDemo/ZYBluetoothPrintDemo/ViewController.m
+++ ZYBluetoothPrintDemo/ZYBluetoothPrintDemo/ViewController.m
@@ -12,6 +12,7 @@
 #import "DemoTool.h"
 
 @interface ViewController ()
+
 @property (weak, nonatomic) IBOutlet UILabel *nameL;
 @property (weak, nonatomic) IBOutlet UITextView *textV;
 @property (nonatomic, strong) ZYBTPrinterTool *tool;
@@ -22,8 +23,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.tool = [ZYBTPrinterTool new];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -39,14 +38,23 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 - (IBAction)selectClick:(id)sender {
     [self.navigationController pushViewController:[BTListTableViewController new] animated:YES];
 }
 
 - (IBAction)printClick:(id)sender {
+    // 蓝牙打印
+    self.tool = [ZYBTPrinterTool new];
+    
+    // 数据生成类
     DemoTool *dataTool = [DemoTool new];
     dataTool.model = @{@"content" : self.textV.text};
+    
+    // 赋值
     self.tool.dataTool = dataTool;
+    
+    // 开始打印
     [self.tool startToPrint];
 }
 
